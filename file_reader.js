35c803a41bf77f3541be9217fbea1509cfe9f8ed
koya <koya9431ptr@gmail.com>
2024-11-19 19:45:39 +0000

    done

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,33 @@
 const fs = require("fs");
 
-fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
+fs.readFile("./firstname.txt", "utf-8", (err, firstName) => {
   // your code here
+  if (err) {
+    console.log(err);
+  } else {
+    // console.log(data1);
+    fs.readFile("./lastname.txt", "utf-8", (err, lastname) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log(data2);
+        fs.readFile("./age.txt", "utf-8", (err, age) => {
+          if (err) {
+            console.log(err);
+          } else {
+            // console.log(data3);
+            fs.readFile("./hobbies.txt", "utf-8", (err, hobbies) => {
+              if (err) {
+                console.log(err);
+              } else {
+                hobbies = JSON.parse(hobbies);
+                // console.log(data4);
+                console.log(firstName + " " + lastname + " " + " is " + age + " years old and his hoobies are " + hobbies[0] + " and " + hobbies[1] + ".");
+              }
+            });
+          }
+        });
+      }
+    });
+  }
 })
